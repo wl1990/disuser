3096a241b8a0baa783479aa79ddc9a7d79a90d98
wl1990 <1209320511@qq.com>
2017-05-02 20:45:13 +0800

    add

--- src/main/java/com/test/controller/Usercontroller.java
+++ src/main/java/com/test/controller/Usercontroller.java
@@ -3,6 +3,7 @@ package com.test.controller;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.alibaba.fastjson.JSONObject;
 import com.test.model.User;
 import com.test.service.UserService;
+import com.test.utils.NebulaException;
 
 @RestController
 public class Usercontroller {
@@ -39,12 +41,30 @@ public class Usercontroller {
     	 return j;
 	}
 	
-	
+	/**
+	 * 
+	 * @param username
+	 * @return
+	 * @throws NebulaException
+	 */
 	@RequestMapping(value="/user/{username}",method=RequestMethod.GET)
-	public JSONObject getUser(@PathVariable("username") String username){
+	public JSONObject getUser(@PathVariable("username") String username) throws NebulaException{
 		 JSONObject j=new JSONObject();
     	 j.put("username", username);
     	 return j;
 	}
+	/**
+	 * @controllerAdvice+@ExceptionHandler 实现exception的统一处理
+	 * 缺点 只能处理controller未处理的异常，对interceptor层的异常，spring框架层的异常，tomcat容器servlet 的异常无法处理
+	 * @throws NebulaException
+	 */
+	@RequestMapping(value="/testException",method=RequestMethod.GET)
+	public void testException() throws NebulaException{
+    	throw new NebulaException(500,"exception test");
+	}
 	
+	@RequestMapping(value="/testExceptionNoFilter",method=RequestMethod.GET)
+	public void testExceptionNoFilter() throws NebulaException{
+    	throw new NebulaException(500,"exception test");
+	}
 }
