32d7c0e0134d69bf1abe9122280eb61fee90d6da
wl1990 <1209320511@qq.com>
2017-09-29 10:48:18 +0800

    spring version

--- src/main/java/com/test/main/AppMain.java
+++ src/main/java/com/test/main/AppMain.java
@@ -1,9 +1,10 @@
 package com.test.main;
 
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
