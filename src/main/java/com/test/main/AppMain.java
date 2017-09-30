8073386556ae4a2c5e592bdca09beec2cd339a14
wl1990 <1209320511@qq.com>
2017-09-30 15:01:28 +0800

    add log

--- src/main/java/com/test/main/AppMain.java
+++ src/main/java/com/test/main/AppMain.java
@@ -3,6 +3,8 @@ package com.test.main;
 
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,9 +14,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication(scanBasePackages={ "com.test.controller"})
 @EnableSwagger2
 public class AppMain {
-	
+	private static Logger log=LoggerFactory.getLogger(AppMain.class);
 	public static void main(String[] args) {
+		try{
+			log.info("-------start disuser------");
 		SpringApplication.run(AppMain.class, args);
+		}catch(Exception e){
+			log.error("--disuser-"+e);
+		}
 	}
 	
 	
