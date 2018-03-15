4d5eb0342bfc837a932827db698bca44ccc256f6
wl1990 <1209320511@qq.com>
2018-03-15 20:33:11 +0800

    springcloud eureka

--- src/main/java/com/test/main/AppMain.java
+++ src/main/java/com/test/main/AppMain.java
@@ -1,28 +1,24 @@
 package com.test.main;
 
-
-
-
-
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ImportResource;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //@ImportResource("classpath:dubbo/spring-provider-dubbo.xml")
-@SpringBootApplication(scanBasePackages={ "com.test.controller","com.test.service"})
+@SpringBootApplication
 @EnableSwagger2
+@EnableEurekaServer
 public class AppMain {
 	private static Logger log=LoggerFactory.getLogger(AppMain.class);
 	public static void main(String[] args) {
 		try{
 			log.info("-------start disuser------");
-		SpringApplication.run(AppMain.class, args);
+		    SpringApplication.run(AppMain.class, args);
 		}catch(Exception e){
 			log.error("--disuser-"+e);
 		}
