9f19221b0bd3d0107fd7840bb03a742fb65f005f
wl1990 <1209320511@qq.com>
2018-03-16 17:34:52 +0800

    first commit

--- src/main/java/com/test/main/AppMain.java
+++ src/main/java/com/test/main/AppMain.java
@@ -10,13 +10,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@ImportResource("classpath:dubbo/spring-provider-dubbo.xml")
-@SpringBootApplication(scanBasePackages={ "com.test.controller","com.test.service"})
-@EnableSwagger2
+
+@SpringBootApplication
+@EnableEurekaServer
 public class AppMain {
 	private static Logger log=LoggerFactory.getLogger(AppMain.class);
 	public static void main(String[] args) {
